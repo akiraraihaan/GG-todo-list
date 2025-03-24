671e50bf91ffbcc705f2ca3b70cbcb0ac65604e9
Akira <raihanakirar@gmail.com>
2025-03-24 15:32:41 +0700

    todo list app - first task of GG

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer>
-      <p>Copyrifgt &copy; 2025 GoGrow</p>
+      <p>Copyrifgt &copy; 2025 GG</p>
       <Link to="/about">About</Link>
     </footer>
   )
